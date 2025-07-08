97596b2785446f6d4a61b54b4339d59c27c95458
Alexandr <79024450444@yandex.ru>
2025-07-09 00:32:33 +0500

    Base and rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+    base: '/',
     plugins: [
         tailwindcss(),
     ],
     build: {
         outDir: 'docs',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                kak_dobavit_3d_model_v_kartochku_tovara: (__dirname, 'tpost/lhkjbinto1-kak-dobavit-3d-model-v-kartochku-tovara.html'),
+                kak_viglyadit_funktsiya_prosmotra_3d_modeli: (__dirname, 'tpost/bspguiymh1-kak-viglyadit-funktsiya-prosmotra-3d-mod.html'),
+            }
+        }
     }
 })
